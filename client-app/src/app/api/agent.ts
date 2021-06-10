3c9d01fdf19122053bfb9208fc0813bb77a319bd
Gurkan123 <aligrknkya61@gmail.com>
2021-06-10 17:43:10 +0300

    End of section 7

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,4 @@
 import axios, { AxiosResponse } from 'axios';
-import { error } from 'console';
-import { resolve } from 'dns';
 import { Activity } from '../models/activity';
 
 const sleep = (delay: number) => {
